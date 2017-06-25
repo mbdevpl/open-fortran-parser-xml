3b8baca3f738387e3087a769a86bbca6b02f722d
Mateusz Bysiek <mb@mbdev.pl>
2017-06-25 19:41:22 +0900

    transforming most common experssions to Python

--- open_fortran_parser/ast_transformer.py
+++ open_fortran_parser/ast_transformer.py
@@ -5,6 +5,7 @@ import typing as t
 import xml.etree.ElementTree as ET
 
 import typed_ast.ast3 as typed_ast3
+import typed_astunparse
 
 _LOG = logging.getLogger(__name__)
 
@@ -21,12 +22,22 @@ class AstTransformer:
     def __init__(self):
         self.transforms = [f for f in dir(self) if not f.startswith('__')]
 
+    def transform(self, node: ET.Element, warn=True):
+        if f'_{node.tag}' not in self.transforms:
+            if warn:
+                _LOG.warning('no transformer available for node "%s"', node.tag)
+                _LOG.debug('%s', ET.tostring(node).decode().rstrip())
+            return None
+        _transform = getattr(self, f'_{node.tag}')
+        return _transform(node)
+
     def transform_all_subnodes(self, node: ET.Element, warn=True):
         transformed = []
         for subnode in node:
             if f'_{subnode.tag}' not in self.transforms:
                 if warn:
-                    _LOG.warning('%s', ET.tostring(subnode).decode().rstrip())
+                    _LOG.warning('no transformer available for node "%s"', subnode.tag)
+                    _LOG.debug('%s', ET.tostring(subnode).decode().rstrip())
                 continue
             _transform = getattr(self, f'_{subnode.tag}')
             transformed.append(_transform(subnode))
@@ -120,6 +131,14 @@ class AstTransformer:
         return typed_ast3.If(test=test, body=body, orelse=[])
 
     def _statement(self, node: ET.Element):
+        details = self.transform_all_subnodes(node, warn=False)
+        if len(details) == 1:
+            if isinstance(details[0], typed_ast3.Assign):
+                return details[0]
+            return typed_ast3.Expr(value=details[0])
+        #else:
+        #    print(details)
+        #    exit(1)
         args = []
         #args.append(typed_ast3.Str(s=ET.tostring(node).decode().rstrip())
         args.append(typed_ast3.Num(n=len(node)))
@@ -127,9 +146,24 @@ class AstTransformer:
             func=typed_ast3.Name(id='print', ctx=typed_ast3.Load()),
             args=args, keywords=[]))
 
+    def _call(self, node: ET.Element):
+        elements = self.transform_all_subnodes(node, warn=False)
+        assert len(elements) == 1
+        func = elements[0]
+        #assert name.tag == 'name' or name.
+        args = []
+        #args = node.findall('./name/subscripts/subscript') if isinstance(name, typed_ast3.Subscript) else []
+        #exit(1)
+        return typed_ast3.Call(func=func, args=args, keywords=[])
+
     def _assignment(self, node: ET.Element):
+        target = self.transform_all_subnodes(node.find('./target'))
+        value = self.transform_all_subnodes(node.find('./value'))
         _LOG.warning('%s', ET.tostring(node).decode().rstrip())
-        raise NotImplementedError()
+        assert len(target) == 1, target
+        assert len(value) == 1, value
+        #raise NotImplementedError()
+        return typed_ast3.Assign(targets=[target], value=value)
 
     def _declaration(self, node: ET.Element):
         _LOG.warning('%s', ET.tostring(node).decode().rstrip())
@@ -143,18 +177,32 @@ class AstTransformer:
 
     def _operation_binary(self, node: ET.Element):
         operand_nodes = node.findall('./operand')
-        assert len(operand_nodes) == 2
+        #if len(operand_nodes) > 2:
+        assert len(operand_nodes) >= 2, operand_nodes
         operands = []
         for operand in operand_nodes:
             new_operands = self.transform_all_subnodes(operand)
-            assert len(new_operands) <= 1 # TODO: change to equality
+            if len(new_operands) != 1:
+                _LOG.warning('%s', ET.tostring(operand).decode().rstrip())
+                #_LOG.error("%s", operand)
+                _LOG.error([typed_astunparse.unparse(_).rstrip() for _ in new_operands])
+                raise SyntaxError()
             operands += new_operands
-        assert len(operands) == 2
+        #assert len(operand_nodes) == len(operands)
+        assert len(operands) >= 2, operands
         operation_type, operator_type = self._operator(node.attrib['operator'])
         if operation_type is typed_ast3.Compare:
+            if len(operands) != 2:
+                raise NotImplementedError('exactly 2 operands expected in comparison operation')
             return typed_ast3.Compare(left=operands[0], ops=[operator_type()], comparators=[operands[1]])
         if operation_type is typed_ast3.BinOp:
-            return typed_ast3.BinOp(left=operands[0], op=operator_type(), right=operands[1])
+            operation = typed_ast3.BinOp(left=operands[0], op=operator_type(), right=None)
+            for operand in operands[1:-1]:
+                operation.right = typed_ast3.BinOp(left=operand, op=operator_type(), right=None)
+                operation = operation.right
+            operation.right = operands[-1]
+            return operation
+            #return typed_ast3.BinOp(left=operands[0], op=operator_type(), right=operands[1])
         _LOG.warning('%s', ET.tostring(node).decode().rstrip())
         raise NotImplementedError(operation_type)
 
@@ -167,6 +215,7 @@ class AstTransformer:
             '/': (typed_ast3.BinOp, typed_ast3.Div),
             '%': (typed_ast3.BinOp, typed_ast3.Mod),
             '**': (typed_ast3.BinOp, typed_ast3.Pow),
+            '//': (typed_ast3.BinOp, typed_ast3.Add), # concatenation operator, only in Fortran
             # LShift
             # RShift
             # BitOr
@@ -193,13 +242,23 @@ class AstTransformer:
         subscripts = []
         for subscript in subscripts_node.findall('./subscript'):
             new_subscripts = self.transform_all_subnodes(subscript)
-            assert len(new_subscripts) <= 1 # TODO: change to equality
-            subscripts += new_subscripts
+            if not new_subscripts:
+                continue
+            if len(new_subscripts) == 1:
+                new_subscript = typed_ast3.Index(value=new_subscripts[0])
+            elif len(new_subscripts) == 2:
+                new_subscript = typed_ast3.Slice(
+                    lower=new_subscripts[0], upper=new_subscripts[1], step=None)
+            else:
+                _LOG.error('%s', ET.tostring(subscript).decode().rstrip())
+                _LOG.error('%s', [typed_astunparse.unparse(_) for _ in new_subscripts])
+                raise SyntaxError()
+            subscripts.append(new_subscript)
+        assert len(subscripts) > 0
         if len(subscripts) == 1:
-            slice_ = typed_ast3.Index(value=subscripts[0])
+            slice_ = subscripts[0]
         else:
-            _LOG.warning('%s', ET.tostring(node).decode().rstrip())
-            raise NotImplementedError()
+            slice_ = typed_ast3.ExtSlice(dims=subscripts)
         return typed_ast3.Subscript(value=name, slice=slice_, ctx=typed_ast3.Load())
 
     def _literal(self, node: ET.Element) -> typed_ast3.AST:
