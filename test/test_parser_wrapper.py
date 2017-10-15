a727f380f6f506093ef937f86e3bf4c6de722cff
Mateusz Bysiek <mb@mbdev.pl>
2017-10-15 13:04:22 +0900

    testing non-default config values

--- test/test_parser_wrapper.py
+++ test/test_parser_wrapper.py
@@ -1,11 +1,15 @@
 """Tests for parser_wrapper module."""
 
+import itertools
 import logging
 import pathlib
+import tempfile
 import unittest
 import xml.etree.ElementTree as ET
 
+from open_fortran_parser.config import JAVA as java_config
 from open_fortran_parser.parser_wrapper import execute_parser, parse
+from .test_setup import run_program
 
 _LOG = logging.getLogger(__name__)
 
@@ -18,6 +22,49 @@ VERBOSITIES = (0, 20, 80, 100)
 class Tests(unittest.TestCase):
 
     maxDiff = None
+
+    def test_config_variants(self):
+        executable = java_config['executable']
+        options = java_config['options']
+        ofp_class = java_config['ofp_class']
+        ofp_xml_class = java_config['ofp_xml_class']
+        input_path = pathlib.Path('test', 'examples', 'empty.f')
+        for classpath, verbosity, invalid_options in itertools.product(
+                (None, java_config['classpath']), (0, 100, 'invalid'),
+                ([], ['--invalid', 'option'], ['--help', 'true'])):
+            with tempfile.NamedTemporaryFile() as output_file:
+                for output_path in (None, output_file.name):
+                    with self.subTest(classpath=classpath, verbosity=verbosity,
+                                      invalid_options=invalid_options, output_path=output_path):
+                        command = [str(executable)]
+                        if classpath is not None:
+                            command += ['-cp', str(classpath)]
+                        if options is not None:
+                            command += options
+                        command.append(ofp_class)
+                        command += ['--class', ofp_xml_class, '--verbosity', str(verbosity)]
+                        if output_path is not None:
+                            command += ['--output', str(output_path)]
+                        command += invalid_options
+                        command.append(str(input_path))
+
+                        if invalid_options or verbosity == 'invalid':
+                            with self.assertRaises(AssertionError):
+                                run_program(*command)
+                            continue
+                        run_program(*command)
+
+    def test_unset_config(self):
+        classpath = java_config['classpath']
+        options = java_config['options']
+
+        input_path = pathlib.Path('test', 'examples', 'empty.f')
+        java_config['classpath'] = None
+        with tempfile.NamedTemporaryFile() as output_file:
+            execute_parser(input_path, pathlib.Path(output_file.name))
+
+        java_config['classpath'] = classpath
+        java_config['options'] = options
 
     def test_execute_parser_stdout(self):
         for input_path in INPUT_PATHS:
