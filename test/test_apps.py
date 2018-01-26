b424be5803e5e9df1ecf0f4e3a53984bd69d49b1
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2018-01-26 14:34:40 +0900

    reorganized FLASH test cases

--- test/test_apps.py
+++ test/test_apps.py
@@ -13,21 +13,38 @@ _HERE = pathlib.Path(__file__).resolve().parent
 
 _APPS_ROOT_PATHS = {
     'miranda_io': pathlib.Path('..', 'miranda_io'),
-    'FLASH': pathlib.Path('..', 'flash-subset', 'FLASH4.4'),
+    'FLASH-4.5': pathlib.Path('..', 'flash-4.5'),
+    'FLASH-SUBSET': pathlib.Path('..', 'flash-subset', 'FLASH4.4'),
     'FFB-MINI': pathlib.Path('..', 'ffb-mini')}
 
-_APPS_OPTIONAL = {'FLASH'}
+_APPS_OPTIONAL = {'FLASH-4.5', 'FLASH-SUBSET'}
 
 _APPS_ROOT_PATHS = {
     app: _HERE.parent.joinpath(path).resolve() for app, path in _APPS_ROOT_PATHS.items()
     if app not in _APPS_OPTIONAL or _HERE.parent.joinpath(path).is_dir()}
 
+_FLASH_COMMON_PATHS = [
+    'physics/Hydro/HydroMain/unsplit/hy_uhd_getFaceFlux.F90',
+    'physics/Hydro/HydroMain/split/MHD_8Wave/hy_8wv_interpolate.F90',
+    'physics/Hydro/HydroMain/split/MHD_8Wave/hy_8wv_fluxes.F90',
+    'physics/Eos/EosMain/Gamma/eos_idealGamma.F90',
+    'physics/Hydro/HydroMain/split/MHD_8Wave/hy_8wv_sweep.F90',
+    'physics/Hydro/HydroMain/unsplit/hy_uhd_DataReconstructNormalDir_MH.F90',
+    'physics/Hydro/HydroMain/unsplit/hy_uhd_upwindTransverseFlux.F90',
+    'physics/Hydro/HydroMain/unsplit/hy_uhd_TVDslope.F90',
+    'physics/Hydro/HydroMain/unsplit/hy_uhd_Roe.F90']
+
 _APPS_CODE_FILEPATHS = {
     'miranda_io': all_fortran_paths(_APPS_ROOT_PATHS['miranda_io']),
-    'FLASH': [pathlib.Path(_APPS_ROOT_PATHS['FLASH'], 'source', pathlib.Path(input_path))
-              for input_path in [
-                  'physics/Hydro/HydroMain/simpleUnsplit/HLL/hy_hllUnsplit.F90'
-                  ]] if 'FLASH' in _APPS_ROOT_PATHS else [],
+    'FLASH-4.5': [
+        pathlib.Path(_APPS_ROOT_PATHS['FLASH-4.5'], 'source', pathlib.Path(input_path))
+        for input_path in [
+            ] + _FLASH_COMMON_PATHS] if 'FLASH-4.5' in _APPS_ROOT_PATHS else [],
+    'FLASH-SUBSET': [
+        pathlib.Path(_APPS_ROOT_PATHS['FLASH-SUBSET'], 'source', pathlib.Path(input_path))
+        for input_path in [
+            'physics/Hydro/HydroMain/simpleUnsplit/HLL/hy_hllUnsplit.F90'
+            ] + _FLASH_COMMON_PATHS] if 'FLASH-SUBSET' in _APPS_ROOT_PATHS else [],
     'FFB-MINI': [path for path in all_fortran_paths(_APPS_ROOT_PATHS['FFB-MINI'].joinpath('src'))
                  if path.name not in ('gfc.h', 'gfrd_c.h', 'gfutil_c.h', 'gfutil_f.h', 'gfwrt_c.h',
                                       'maprof.h', 'maprof_proc.h', 'maprof_yaml.h')]}
@@ -57,9 +74,13 @@ class Tests(unittest.TestCase):
     def test_miranda_io(self):
         self._run_app_test('miranda_io')
 
-    @unittest.skipIf('FLASH' not in _APPS_ROOT_PATHS, 'FLASH directory not found')
-    def test_flash(self):
-        self._run_app_test('FLASH', 'flash')
+    @unittest.skipIf('FLASH-4.5' not in _APPS_ROOT_PATHS, 'FLASH-4.5 directory not found')
+    def test_flash_45(self):
+        self._run_app_test('FLASH-4.5', 'flash-4.5')
+
+    @unittest.skipIf('FLASH-SUBSET' not in _APPS_ROOT_PATHS, 'FLASH-SUBSET directory not found')
+    def test_flash_subset(self):
+        self._run_app_test('FLASH-SUBSET', 'flash-subset')
 
     def test_ffb_mini(self):
         self._run_app_test('FFB-MINI', 'ffb-mini', 24)
