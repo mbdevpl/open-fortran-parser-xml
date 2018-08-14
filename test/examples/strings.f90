5363346018fb05e18ef7981e471093ca20afea21
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2018-08-14 13:31:47 -0500

    new test case

---
+++ test/examples/strings.f90
@@ -0,0 +1,16 @@
+program strings
+
+  implicit none
+
+  character(*) :: mystring01a
+  character(16) :: mystring01b
+  character(len=*) :: mystring02a
+  character(len=16) :: mystring02b
+  character(len=*, kind=c_char) :: mystring03a
+  character(len=16, kind=c_char) :: mystring03b
+  character :: mystring04a(*)
+  character :: mystring04b(16)
+  character(kind=c_char) :: mystring05a(*)
+  character(kind=c_char) :: mystring05b(16)
+
+end program strings
