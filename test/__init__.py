76a7bb465af280ac725d3177ba1c7ac655bcc62d
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2019-04-02 15:48:12 +0900

    logging adjustment

--- test/__init__.py
+++ test/__init__.py
@@ -10,6 +10,8 @@ if java_config['options'] is None:
     java_config['options'] = []
 java_config['options'].append('-Djava.util.logging.config.file=logging.properties')
 
+os.makedirs(str(pathlib.Path('test', 'results', 'logs')), exist_ok=True)
+
 if os.environ.get('TEST_COVERAGE'):
     JACOCO_PATH = pathlib.Path('lib', 'org.jacoco.agent-0.8.1-runtime.jar').resolve()
     JACOCO_EXCLUDES = ('fortran.ofp.parser.java.FortranParserExtras_FortranParser08',)
